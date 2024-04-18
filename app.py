06d4eb6fd12881b41d6b7e8130ebd985cf5861b1
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-04-18 16:41:29 +0530

    init: can take contact info and serve them back

--- app.py
+++ app.py
@@ -5,10 +5,10 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.exceptions import NotFound
 from flask_cors import CORS
 
-app = Flask(__name__)
-CORS(app,resources={r"/*":{"origins":"https://thanaykumaryr.github.io/*"}})
+app1 = Flask(__name__)
+CORS(app1,resources={r"/*":{"origins":"https://thanaykumaryr.github.io/*"}})
 
-@app.route('/contact',methods=['GET','POST'])
+@app1.route('/contact',methods=['GET','POST'])
 def index():
     if(request.method == 'POST'): 
         name = request.json["name"]
@@ -20,9 +20,9 @@ def index():
         return str(f"{name} working with {company if company else 'No Comapany'} has email {email} and number {number} with message: \n'{message}.'")
     return "<h1>Welcome to Nutrispy Server</h1>"
 
-hostedApp = Flask(__name__)
+app = Flask(__name__)
 
-hostedApp.wsgi_app = DispatcherMiddleware(NotFound,{"/api/v1":app})
+app.wsgi_app = DispatcherMiddleware(NotFound,{"/api/v1":app1})
 
 # if __name__ == "__main__":
 #     hostedApp.run(port=30000,host="0.0.0.0",debug=True)
