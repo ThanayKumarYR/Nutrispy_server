24ce68e93f7c57893be1236585b92edddc4d5e5e
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-05-04 17:49:07 +0530

    add: functionality decline login again if logined

--- Controllers/loginSystem.py
+++ Controllers/loginSystem.py
@@ -6,20 +6,27 @@ import json
 
 def login():
     if request.method == 'POST':
+        if 'user' in session:
+            return jsonify({"response": "Failed", "statusCode": 400, "data": "User is already logged in"})
+        
         email = request.json['email']
         password = request.json['password']
-        print("email = "+email+", password = "+password)
+        
         try:
             config = json.loads(os.getenv("FIREBASE"))
             firebase = initialize_app(config)
             authenticate = firebase.auth()
-            user = auth.get_user_by_email(email=email)
+            
+            user = authenticate.get_user_by_email(email=email)
+            
             if user:
-                auth_user =  authenticate.sign_in_with_email_and_password(email, password)
+                auth_user = authenticate.sign_in_with_email_and_password(email, password)
                 session['user'] = auth_user['localId']
-            return jsonify({"response":"Success","statusCode":200,"data":f"Successfully login, Welcome {auth_user['email']}"})
+                
+            return jsonify({"response": "Success", "statusCode": 200, "data": f"Successfully logged in. Welcome {auth_user['email']}"})
+        
         except Exception as e:
-             return jsonify({"response":"Failed","statusCode":404,"data":e.args[0]})
+            return jsonify({"response": "Failed", "statusCode": 404, "data": e.args[0]})
             
 def logout():
     if 'user' in session:
