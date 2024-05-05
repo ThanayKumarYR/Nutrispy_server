afa17bb909c66d06dcf9e0eae47e95eb1d699e13
Thanay Kumar Y R <thanaykumaryr@gmail.com>
2024-05-05 21:50:22 +0530

    admin cannot access excerciseRecomenation

--- Controllers/exercise_recommendation.py
+++ Controllers/exercise_recommendation.py
@@ -2,7 +2,9 @@ from flask import session,jsonify
 from Utilities import is_admin
 
 def exercise_recommendation():
-    if 'user' in session and not is_admin():
+    if is_admin():
+        return jsonify({"response": "unauthorized", "statusCode": 401, "data": "Admins cannot access this route"})
+    elif 'user' in session:
         return "<h1>Welcome to exercise recommendation</h1>", 200
     else:
         return jsonify({"response": "unauthorized", "statusCode": 401, "data": "Login to use this feature"})
